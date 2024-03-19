74c281b628ed835dc83946a2ed94833dd4bc8813
Xunnamius (Romulus) <Xunnamius@users.noreply.github.com>
2024-03-19 02:07:07 -0700

    chore(src): cosmetic

--- src/error.ts
+++ src/error.ts
@@ -206,7 +206,7 @@ export const ErrorMessage = {
     return 'this command is currently unimplemented';
   },
   InvalidSubCommandInvocation() {
-    return 'invalid sub-command: you must call this command with a valid sub-command argument';
+    return 'invalid sub-command: you must call this with a valid sub-command argument';
   },
   FrameworkError(error: unknown) {
     return `UNHANDLED FRAMEWORK EXCEPTION: an error occurred due to a misconfiguration. This is typically due to developer error and as such cannot be fixed by end-users. Please report this incident to the developer of this application.\nError: ${error}`;
