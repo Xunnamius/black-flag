f3086f07a0f4cf661850599e370f220c47febbd1
Xunnamius (Romulus) <Xunnamius@users.noreply.github.com>
2025-03-24 21:12:21 -0700

    Revert "refactor(test): regenerate asset"
    This reverts commit dc1ac03433006d4ed2c815a3d9a74c2b066f2dcf.

--- test/setup.ts
+++ test/setup.ts
@@ -3,16 +3,8 @@
  **  bootstrapping the runtime for every test file.
  */
 
-// ? These will always come from @-xun/symbiote and @-xun/jest (transitively)
-// {@symbiote/notInvalid
-//   - @-xun/jest
-//   - @-xun/test-mock-argv
-//   - @-xun/test-mock-exit
-//   - @-xun/test-mock-import
-//   - @-xun/test-mock-env
-//   - @-xun/test-mock-fixture
-//   - @-xun/test-mock-output
-// }
+// ? jest-extended will always come from @-xun/symbiote (i.e. transitively)
+// {@symbiote/notInvalid jest-extended}
 
 // ? https://github.com/jest-community/jest-extended#typescript
 import 'jest-extended';
